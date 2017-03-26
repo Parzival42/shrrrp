972c4bf23c5753dc0bcfd5dab13bc9a95f1693b6
Parzival Legmaier <legmaier@gmx.at>
2017-03-26 15:19:50 +0200

    2D Blend Tree tests.

---
+++ Assets/Mecanim Demo/TestControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestControllerScript : MonoBehaviour
+{
+    private Animator animator;
+
+	private void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	private void Update ()
+    {
+        HandleInput();
+	}
+
+    private void HandleInput()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("VerticalSpeed", vertical);
+        animator.SetFloat("HorizontalSpeed", horizontal);
+    }
+}
