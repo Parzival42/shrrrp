0f1370473d6514d9c12ecb8f59926d062128626b
Parzival Legmaier <legmaier@gmx.at>
2017-04-05 18:02:20 +0200

    Outlined Dash Handler.

---
+++ Assets/Scripts/Input/DashHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public delegate void DashStartedHandler();
+public delegate void DashCollisionHandler(GameObject self, GameObject other);
+
+public interface DashHandler
+{
+    #region Events
+    event DashStartedHandler OnDashStarted;
+    event DashCollisionHandler OnDashCollision;
+    #endregion
+
+    void PerformDash();
+}
---
+++ Assets/Scripts/Input/RigidBodyDashHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyDashHandler : DashHandler
+{
+    #region Events
+    public event DashCollisionHandler OnDashCollision;
+    public event DashStartedHandler OnDashStarted;
+    #endregion
+
+    public void PerformDash()
+    {
+        // TODO: Implement shit
+    }
+
+    #region Event methods
+    private void OnPerformStart()
+    {
+        if (OnDashStarted != null)
+            OnDashStarted();
+    }
+
+    private void OnPerformDashCollision(GameObject self, GameObject other)
+    {
+        if (OnDashCollision != null)
+            OnDashCollision(self, other);
+    }
+    #endregion
+}
