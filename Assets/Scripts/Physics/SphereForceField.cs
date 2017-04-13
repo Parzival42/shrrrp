7c222000b89d500cd238a85d5fdcac202f1eae8c
Parzival Legmaier <legmaier@gmx.at>
2017-04-13 15:15:15 +0200

    Added:  - Small ForceField framework  - Spherical (actually cylindrical) force field implementation which uses a sphere collider for object detection and a cylindrical force calculation.
    Updated:
     - Added SphericalForceField to PerspectiveTest.untiy

---
+++ Assets/Scripts/Physics/SphereForceField.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class SphereForceField : ForceField
+{
+    #region Inspector variables
+    [Header("Sphere Force Field")]
+    [SerializeField]
+    [Tooltip("Force strength dependent on the distance to the middle point or not.")]
+    private bool uniformForce = false;
+    #endregion
+
+    #region Internal Members
+    private static readonly int DEBUG_ITERATIONS = 8;
+    private SphereCollider sphereCollider;
+
+    Vector2 middlePosition = Vector2.zero;
+    Vector2 bodyPosition = Vector2.zero;
+    #endregion
+
+    private void Start ()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+	}
+
+    protected override Vector3 GetForceFor(Vector3 position)
+    {
+        Vector3 worldColliderCenter = GetWorldColliderCenter();
+        // Only XZ plane starting from the circle collider middle point
+        middlePosition.Set(worldColliderCenter.x, worldColliderCenter.z);
+        bodyPosition.Set(position.x, position.z);
+
+        // Calculate direction and normalize it based on the collider radius
+        Vector2 forceDirection = bodyPosition - middlePosition;
+        forceDirection *= 1f / sphereCollider.radius;
+
+        // Save into 3D result vector
+        Vector3 resultVector = new Vector3(forceDirection.x, 0f, forceDirection.y);
+        if (uniformForce)
+            resultVector.Normalize();
+
+        return resultVector;
+    }
+
+    protected override void DrawDebugVisualization()
+    {
+        float distanceStep = sphereCollider.radius / DEBUG_ITERATIONS;
+        Vector3 worldColliderCenter = GetWorldColliderCenter();
+
+        // Red shape circles
+        DebugExtension.DebugCircle(worldColliderCenter, Vector3.up, Color.red, 0.5f);
+        DebugExtension.DebugCircle(worldColliderCenter + Vector3.up * 0.3f, Vector3.up, Color.red, sphereCollider.radius);
+        DebugExtension.DebugCircle(worldColliderCenter + Vector3.down * 0.3f, Vector3.up, Color.red, sphereCollider.radius);
+
+        // Arrows
+        for (int i = 0; i < DEBUG_ITERATIONS; i++)
+        {
+            Vector3 forward = sphereCollider.transform.forward;
+            float distanceFromCenter = distanceStep * (i + 1);
+
+            for (float angle = 0; angle <= 360f; angle += 20f)
+            {
+                Vector3 position = worldColliderCenter + forward * distanceFromCenter;
+                Vector3 force = GetForceFor(position);
+                DebugExtension.DebugArrow(position, force, GetMagnitudeColor(force));
+
+                forward = Quaternion.Euler(0f, angle, 0f) * sphereCollider.transform.forward;
+                forward.Normalize();
+            }
+        }
+    }
+
+    private Color GetMagnitudeColor(Vector3 force)
+    {
+        float magnitude = force.magnitude;
+        return Color.Lerp(Color.green, Color.red, magnitude);
+    }
+
+    private Vector3 GetWorldColliderCenter()
+    {
+        return sphereCollider.transform.TransformPoint(sphereCollider.center);
+    }
+}
