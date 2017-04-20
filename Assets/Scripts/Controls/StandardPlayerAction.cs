9ec8891fb47fc1e5d4f7b3114b53235108e34a8e
Parzival Legmaier <legmaier@gmx.at>
2017-04-20 10:43:07 +0200

    Added InControl.

---
+++ Assets/Scripts/Controls/StandardPlayerAction.cs
@@ -0,0 +1,27 @@
+﻿using InControl;
+
+public class StandardPlayerAction : PlayerActionSet
+{
+    public PlayerAction Left;
+    public PlayerAction Right;
+    public PlayerAction Up;
+    public PlayerAction Down;
+
+    public PlayerAction Jump;
+    public PlayerAction Dash;
+
+    public PlayerTwoAxisAction Move;
+
+    public StandardPlayerAction()
+    {
+        Left = CreatePlayerAction("Move Left");
+        Right = CreatePlayerAction("Move Right");
+        Up = CreatePlayerAction("Move Up");
+        Down = CreatePlayerAction("Move Down");
+
+        Jump = CreatePlayerAction("Jump");
+        Dash = CreatePlayerAction("Dash");
+
+        Move = CreateTwoAxisPlayerAction(Left, Right, Down, Up);
+    }
+}
