e0e5c16dfa1b9b182f4ff94928db7d98ead8b9a5
Parzival Legmaier <legmaier@gmx.at>
2017-04-20 10:59:42 +0200

    Finalized default binding.

--- Assets/Scripts/Controls/StandardPlayerAction.cs
+++ Assets/Scripts/Controls/StandardPlayerAction.cs
@@ -10,6 +10,8 @@ public class StandardPlayerAction : PlayerActionSet
     public PlayerAction Jump;
     public PlayerAction Dash;
 
+    public PlayerAction Pause;
+
     public PlayerTwoAxisAction Move;
 
     public StandardPlayerAction()
@@ -22,6 +24,44 @@ public class StandardPlayerAction : PlayerActionSet
         Jump = CreatePlayerAction("Jump");
         Dash = CreatePlayerAction("Dash");
 
+        Pause = CreatePlayerAction("Pause");
+
         Move = CreateTwoAxisPlayerAction(Left, Right, Down, Up);
+    }
+
+    public static StandardPlayerAction CreateStandardBinding()
+    {
+        StandardPlayerAction playerAction = new StandardPlayerAction();
+
+        // Left
+        playerAction.Left.AddDefaultBinding(InputControlType.LeftStickLeft);
+        playerAction.Left.AddDefaultBinding(InputControlType.DPadLeft);
+
+        // Right
+        playerAction.Right.AddDefaultBinding(InputControlType.LeftStickRight);
+        playerAction.Right.AddDefaultBinding(InputControlType.DPadRight);
+
+        // Up
+        playerAction.Up.AddDefaultBinding(InputControlType.LeftStickUp);
+        playerAction.Up.AddDefaultBinding(InputControlType.DPadUp);
+
+        // Down
+        playerAction.Down.AddDefaultBinding(InputControlType.LeftStickDown);
+        playerAction.Down.AddDefaultBinding(InputControlType.DPadDown);
+
+        // Jump
+        playerAction.Jump.AddDefaultBinding(InputControlType.Action1);
+        playerAction.Jump.AddDefaultBinding(InputControlType.Action2);
+
+        // Dash
+        playerAction.Dash.AddDefaultBinding(InputControlType.Action3);
+        playerAction.Dash.AddDefaultBinding(InputControlType.Action4);
+
+        // Xbox one specific
+        playerAction.Pause.AddDefaultBinding(InputControlType.Menu);
+        // Xbox 360 specific
+        playerAction.Pause.AddDefaultBinding(InputControlType.Start);
+
+        return playerAction;
     }
 }
