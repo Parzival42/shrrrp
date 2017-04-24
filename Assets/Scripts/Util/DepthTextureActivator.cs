ac4dccb87498059d3bd78b1dfe8cc2ccf9f83b05
Parzival Legmaier <legmaier@gmx.at>
2017-04-24 19:05:44 +0200

    Intersection test shader (Not working, don't know why :/) Maybe something changed in a new unity version

---
+++ Assets/Scripts/Util/DepthTextureActivator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class DepthTextureActivator : MonoBehaviour
+{
+    [Comment("The only purpose of this script is to activate set the depth mode of the camera."
+        + "Some scripts require the depth to be written.")]
+
+    [FancyHeader("Depth Mode", "Choose a depth mode!")]
+    [SerializeField]
+    private DepthTextureMode depthMode = DepthTextureMode.DepthNormals;
+
+	private void Start ()
+    {
+        Camera cam = GetComponent<Camera>();
+        cam.depthTextureMode = depthMode;
+    }
+}
