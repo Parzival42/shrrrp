948896f0f71ad8e5775a0bdc6f16bd6bd05a2cfe
Parzival Legmaier <legmaier@gmx.at>
2017-04-27 11:00:13 +0200

    Added small trigger framework for later plane cutting spawning.

---
+++ Assets/Scripts/Trigger/PlaneCutterSpawnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlaneCutterSpawnTrigger : TriggerAction
+{
+    [FancyHeader("Plane cutter spawn settings", "Specific settings for plane cutting")]
+    [SerializeField]
+    private GameObject planeCutter;
+
+    protected override void PerformOnTriggerAction(Collider other)
+    {
+        // TODO: Spawn plane cutter
+        // TODO: Handle control stuff
+    }
+}
---
+++ Assets/Scripts/Trigger/TriggerAction.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public abstract class TriggerAction : MonoBehaviour
+{
+    [Comment("This script performs a specific task when a GameObject with a given tag entered a trigger.")]
+
+    [FancyHeader("General settings")]
+    [SerializeField]
+    protected string collisionTag = "Player";
+
+    [SerializeField]
+    protected bool destroyAfterTriggering = false;
+
+	protected virtual void Start ()
+    {
+        CheckIfTriggerAvailable();
+	}
+
+    private void CheckIfTriggerAvailable()
+    {
+        bool foundTrigger = false;
+        Collider[] allCollider = GetComponents<Collider>();
+        foreach(Collider c in allCollider) {
+            if (c.isTrigger)
+                foundTrigger = true;
+        }
+
+        if (!foundTrigger)
+            Debug.LogError("No trigger attached to this GameObject!", gameObject);
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == collisionTag)
+        {
+            PerformOnTriggerAction(other);
+            if (destroyAfterTriggering)
+                Destroy(gameObject);
+        }
+    }
+
+    protected abstract void PerformOnTriggerAction(Collider other);
+}
