b6f49c2b5a28f28e946c4b51c73389adeff4cd91
Parzival Legmaier <legmaier@gmx.at>
2017-05-02 10:46:21 +0200

    Added Right Stick controls to the StandardPlayerAction.

--- Assets/Scripts/Controls/StandardPlayerAction.cs
+++ Assets/Scripts/Controls/StandardPlayerAction.cs
@@ -2,17 +2,26 @@
 
 public class StandardPlayerAction : PlayerActionSet
 {
+    #region Left Stick
     public PlayerAction Left;
     public PlayerAction Right;
     public PlayerAction Up;
     public PlayerAction Down;
+    public PlayerTwoAxisAction Move;
 
     public PlayerAction Jump;
     public PlayerAction Dash;
 
     public PlayerAction Pause;
+    #endregion
 
-    public PlayerTwoAxisAction Move;
+    #region Right Stick
+    public PlayerAction RightStickLeft;
+    public PlayerAction RightStickRight;
+    public PlayerAction RightStickUp;
+    public PlayerAction RightStickDown;
+    public PlayerTwoAxisAction RightStick;
+    #endregion
 
     public PlayerAction NullAction = null;
 
@@ -22,13 +31,18 @@ public class StandardPlayerAction : PlayerActionSet
         Right = CreatePlayerAction("Move Right");
         Up = CreatePlayerAction("Move Up");
         Down = CreatePlayerAction("Move Down");
-        
+        Move = CreateTwoAxisPlayerAction(Left, Right, Down, Up);
+
+        RightStickLeft = CreatePlayerAction("Right Stick Left");
+        RightStickRight = CreatePlayerAction("Right Stick Right");
+        RightStickUp = CreatePlayerAction("Right Stick Up");
+        RightStickDown = CreatePlayerAction("Right Stick Down");
+        RightStick = CreateTwoAxisPlayerAction(RightStickLeft, RightStickRight, RightStickDown, RightStickUp);
+
         Jump = CreatePlayerAction("Jump");
         Dash = CreatePlayerAction("Dash");
 
         Pause = CreatePlayerAction("Pause");
-
-        Move = CreateTwoAxisPlayerAction(Left, Right, Down, Up);
     }
 
     public bool IsNullAction()
@@ -53,17 +67,29 @@ public class StandardPlayerAction : PlayerActionSet
         playerAction.Left.AddDefaultBinding(InputControlType.LeftStickLeft);
         playerAction.Left.AddDefaultBinding(InputControlType.DPadLeft);
 
+        // Right Stick Left
+        playerAction.RightStickLeft.AddDefaultBinding(InputControlType.RightStickLeft);
+
         // Right
         playerAction.Right.AddDefaultBinding(InputControlType.LeftStickRight);
         playerAction.Right.AddDefaultBinding(InputControlType.DPadRight);
+
+        // Right Stick Right
+        playerAction.RightStickRight.AddDefaultBinding(InputControlType.RightStickRight);
 
         // Up
         playerAction.Up.AddDefaultBinding(InputControlType.LeftStickUp);
         playerAction.Up.AddDefaultBinding(InputControlType.DPadUp);
 
+        // Right Stick Up
+        playerAction.RightStickUp.AddDefaultBinding(InputControlType.RightStickUp);
+
         // Down
         playerAction.Down.AddDefaultBinding(InputControlType.LeftStickDown);
         playerAction.Down.AddDefaultBinding(InputControlType.DPadDown);
+
+        // Right Stick Down
+        playerAction.RightStickDown.AddDefaultBinding(InputControlType.RightStickDown);
 
         // Jump
         playerAction.Jump.AddDefaultBinding(InputControlType.Action1);
@@ -93,20 +119,20 @@ public class StandardPlayerAction : PlayerActionSet
         StandardPlayerAction playerAction = new StandardPlayerAction();
 
         // Left
-        playerAction.Left.AddDefaultBinding(Key.LeftArrow);
         playerAction.Left.AddDefaultBinding(Key.A);
+        playerAction.RightStickLeft.AddDefaultBinding(Key.LeftArrow);
 
         // Right
-        playerAction.Right.AddDefaultBinding(Key.RightArrow);
         playerAction.Right.AddDefaultBinding(Key.D);
+        playerAction.RightStickRight.AddDefaultBinding(Key.RightArrow);
 
         // Up
-        playerAction.Up.AddDefaultBinding(Key.UpArrow);
         playerAction.Up.AddDefaultBinding(Key.W);
+        playerAction.RightStickUp.AddDefaultBinding(Key.UpArrow);
 
         // Down
-        playerAction.Down.AddDefaultBinding(Key.DownArrow);
         playerAction.Down.AddDefaultBinding(Key.S);
+        playerAction.RightStickDown.AddDefaultBinding(Key.DownArrow);
 
         // Jump
         playerAction.Jump.AddDefaultBinding(Key.Space);
