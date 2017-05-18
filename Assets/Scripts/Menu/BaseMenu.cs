f8c066969719f9036de061032a3b7889577620ef
Ju Lia <jul.ya@gmx.at>
2017-05-18 16:18:47 +0200

    menu wip (will probably change to multiscenesetup)

---
+++ Assets/Scripts/Menu/BaseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Prime31.TransitionKit;
+
+public abstract class BaseMenu : MonoBehaviour {
+
+	
+    [SerializeField]
+    private Shader transitionShader;
+
+	/// <summary>
+	/// Performs the switch to next scene.
+	/// </summary>
+	public virtual void LoadNextLevel(string levelName){
+		PerformSceneSwitch(levelName);
+	}
+
+	/// <summary>
+	/// Performs the switch to previous level or exits the game.
+	/// </summary>
+	public virtual void LoadPreviousLevel(string levelName){
+		PerformSceneSwitch(levelName);
+	}
+
+	/// <summary>
+	/// Transitions between the scenes.
+	/// </summary>
+	public virtual void PerformSceneSwitch(string levelName)
+    {
+        FishEyeTransition fishEye = new FishEyeTransition()
+        {
+            nextScene = levelName,
+            duration = 0.2f,
+            size = 0.0f,
+            zoom = 10.0f,
+            colorSeparation = 5.0f,
+            fishEyeShader = transitionShader
+        };
+
+        TransitionKit.instance.transitionWithDelegate(fishEye);
+    }
+
+}
---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MainMenu : BaseMenu {
+
+	void Start () {
+
+		// Find the MenuSelectionContainer and reset it OR initialize it if not found
+		MenuSelectionContainer menuSelectionContainer = (MenuSelectionContainer)FindObjectOfType(typeof(MenuSelectionContainer));
+
+		if(menuSelectionContainer != null){
+			menuSelectionContainer.ResetContainer();
+		} else {
+			GameObject containerObject = new GameObject();
+			containerObject.name = "MenuSelectionContainer";
+			containerObject.AddComponent(typeof(MenuSelectionContainer));
+		}
+	}
+
+	/// <summary>
+	/// Exits the game and ignores the parameter given.
+	/// </summary>
+	public override void LoadPreviousLevel(string levelName){
+		Application.Quit();
+	}
+	
+}
