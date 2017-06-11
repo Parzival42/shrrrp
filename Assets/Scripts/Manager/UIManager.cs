a1dcb3a54a359a79535dabceb911ac9e23523452
Parzival Legmaier <legmaier@gmx.at>
2017-06-11 17:59:24 +0200

    Added basic user interface for blending in text inserts for death, win and game start situations. Just drop the 'IngameUserInterface' prefab into the scene and it should already work (in combination with the _ManagerContainer).

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [Comment("Handles UI inserts to comment player deaths and the general win situation during the game.")]
+
+    private static readonly string INSERT_TAG = "TextInsert";
+
+    #region Inspector Variables
+    [FancyHeader("Settings", "UI Manager Settings")]
+    [SerializeField]
+    private bool showIngameUI = true;
+
+    [FancyHeader("Text inserts", "Specific inserts during gameplay")]
+    [SerializeField]
+    private string insertStart = "Cut!";
+
+    [SerializeField]
+    private float startFadeOut = 2f;
+
+    [Space(5)]
+    [SerializeField]
+    private string insertEnd = "Cut into pieces!";
+
+    [SerializeField]
+    private float endScale = 20f;
+
+    [SerializeField]
+    private float endTweenTime = 0.2f;
+
+    [FancyHeader("Tween settings")]
+    [SerializeField]
+    private float tweenTime = 0.2f;
+
+    [SerializeField]
+    private LeanTweenType easeType = LeanTweenType.easeOutBack;
+    #endregion
+
+    #region Internal Variables
+    private PlayerManager playerManager;
+    private Canvas mainCanvas;
+    private Text textInsert;
+    private LTDescr textInsertTween;
+    #endregion
+
+    private void Start()
+    {
+        InitializeUI();
+        InitializePlayerManager();
+    }
+
+    private void InitializePlayerManager()
+    {
+        playerManager = FindObjectOfType<PlayerManager>();
+
+        if (playerManager == null)
+            Debug.LogError("[UIManager]: No PlayerManager found!", gameObject);
+        else
+        {
+            playerManager.OnAllPlayersDied += HandleAllPlayerDead;
+            playerManager.OnOnePlayerLeft += HandleOnePlayerLeft;
+            playerManager.OnPlayerDied += HandlePlayerDied;
+            playerManager.OnAllPlayersFound += HandleAllPlayersFound;
+        }
+    }
+
+    private void InitializeUI()
+    {
+        mainCanvas = GetComponentInChildren<Canvas>();
+        if (mainCanvas == null)
+            Debug.LogError("[UIManager]: No canvas found!.", gameObject);
+
+        textInsert = mainCanvas.gameObject.FindComponentInChildWithTag<Text>(INSERT_TAG);
+        if (textInsert == null)
+            Debug.LogError("[UIManager]: No text insert object found!", gameObject);
+
+        SetTextInsertAlpha(0f);
+    }
+
+    /// <summary>
+    /// Handles start text animation.
+    /// </summary>
+    private void HandleAllPlayersFound(Player[] players)
+    {
+        if (showIngameUI)
+        {
+            textInsert.text = insertStart;
+            textInsertTween = LeanTween.value(textInsert.gameObject, 0f, 1f, tweenTime)
+                .setOnUpdate((float value) => {
+                    SetTextInsertAlpha(value);
+                })
+                .setEase(easeType)
+                .setOnComplete(() => {
+                    textInsertTween = LeanTween.value(textInsert.gameObject, 1f, 0f, tweenTime)
+                        .setOnUpdate((float value) => {
+                            SetTextInsertAlpha(value);
+                        })
+                        .setDelay(startFadeOut);
+                });
+        }
+    }
+
+    private void HandleOnePlayerLeft(Player winner)
+    {
+        if (showIngameUI)
+        {
+            DoPlayerWinTextInsert();
+        }
+    }
+
+    private void HandlePlayerDied(Player deadPlayer)
+    {
+        if (showIngameUI)
+        {
+        }
+    }
+
+    private void HandleAllPlayerDead()
+    {
+        if (showIngameUI)
+        {
+            DoPlayerWinTextInsert();
+        }
+    }
+
+    private void DoPlayerWinTextInsert()
+    {
+        if (textInsertTween != null)
+            LeanTween.cancel(textInsertTween.uniqueId);
+
+        textInsert.text = insertEnd;
+        SetTextInsertAlpha(1f);
+        textInsert.rectTransform.localScale = new Vector2(endScale, endScale);
+
+        textInsertTween = LeanTween.scale(textInsert.rectTransform, Vector2.one, endTweenTime).setEase(LeanTweenType.easeOutBack);
+    }
+
+    private void SetTextInsertColor(Color c)
+    {
+        textInsert.color = c;
+    }
+
+    private void SetTextInsertAlpha(float alpha)
+    {
+        textInsert.color = new Color(textInsert.color.r, textInsert.color.g, textInsert.color.b, alpha);
+    }
+}
---
+++ Assets/Scripts/Util/GameObjectExtension.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class GameObjectExtension
+{
+    /// <summary>
+    /// Returns the component with the given tag. Returns null if nothing was found.
+    /// </summary>
+    /// <param name="parent">Parent gameobject in the hierarchy.</param>
+    /// <param name="tag">Tag</param>
+    /// <returns>First componentn found with the specified tag.</returns>
+    public static T FindComponentInChildWithTag<T>(this GameObject parent, string tag) where T : Component
+    {
+        Transform t = parent.transform;
+        foreach (Transform tr in t)
+            if (tr.tag == tag)
+                return tr.GetComponent<T>();
+
+        return null;
+    }
+}
