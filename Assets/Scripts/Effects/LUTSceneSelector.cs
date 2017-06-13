8ba508156701dd661308c81b81dc7c5206766782
Parzival Legmaier <legmaier@gmx.at>
2017-06-13 14:48:38 +0200

    LUT's can now be set per scene (Use LUTSceneSelector) A lot of LUT's in the post processing pofiles are now set to a neutral LUT.

---
+++ Assets/Scripts/Effects/LUTSceneSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing.Utilities;
+using UnityEngine.PostProcessing;
+
+[RequireComponent(typeof(Camera), typeof(PostProcessingController))]
+public class LUTSceneSelector : MonoBehaviour
+{
+    [FancyHeader("Scene LUT", "Scene specific LUT")]
+    [SerializeField]
+    private Texture2D sceneLUT;
+
+    private PostProcessingController controller;
+
+	private void Start ()
+    {
+        controller = GetComponent<PostProcessingController>();
+        ChangeToSceneLUT();
+	}
+
+    private void ChangeToSceneLUT()
+    {
+        controller.controlUserLut = true;
+        controller.enableUserLut = true;
+        controller.userLut.lut = sceneLUT;
+    }
+}
