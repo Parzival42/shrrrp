e7bae41334cbde5056a2bd323691bfa4bb54e7db
Parzival Legmaier <legmaier@gmx.at>
2017-06-18 18:39:39 +0200

    Modified Artifact for better visibility. (Added cylinder thingy)

---
+++ Assets/Scripts/Util/SimpleRotate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRotate : MonoBehaviour
+{
+    public Vector3 rotation = Vector3.zero;
+
+    [SerializeField]
+    [Tooltip("Activates a time based rotation.")]
+    protected bool timeBasedRotation = false;
+
+    [SerializeField]
+    protected float rotateInterval = 0.3f;
+
+    protected float currentInterval = 0f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!timeBasedRotation)
+            transform.Rotate(rotation);
+        else
+        {
+            if (currentInterval >= rotateInterval)
+            {
+                transform.Rotate(rotation);
+                currentInterval = 0;
+            }
+
+            currentInterval += Time.deltaTime;
+        }
+	}
+}
